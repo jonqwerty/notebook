6fd742d09bd1e24403b90d0143a992ef8ec08d4b
Your Name <you@example.com>
2021-01-02 20:08:45 +0200

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,34 @@
+
+import React, {useState, useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
+
+export const Form = () => {
+    const [value, setValue] = useState('')
+    const alert = useContext(AlertContext)
+
+    const submitHandler = event => {
+        event.preventDefault()
+
+        if (value.trim()) {
+            alert.show('Note was created', 'success')
+            setValue('')
+        } else {
+            alert.show('Enter some  note')
+        }
+
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    className="form-control"
+                    placeholder="Enter name of note"
+                    value={value}
+                    onChange={e => setValue(e.target.value)} />
+            </div>
+        </form>
+    )
+}
+
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React  from 'react'
+
+
+export const About = () => {
+    return (
+        <div className="jumbotron ">
+            <div className="container">
+                <h1 className="display-4">Notes</h1>
+                <p className="lead">Version <strong>1.0.1</strong></p>
+            </div>
+        </div>
+    )
+}
