c76148a210fc8bb5962c8153e9d4828b7f327bd3
Your Name <you@example.com>
2021-01-03 12:32:03 +0200

    worked app

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,22 +1,34 @@
 import React, {useContext} from 'react'
+import {CSSTransition} from 'react-transition-group'
 import { AlertContext } from '../context/alert/alertContext'
 
 
 export const Alert = () => {
     const {alert, hide} = useContext(AlertContext)
 
-    if (!alert.visible) {
-        return null
-    }
+    // if (!alert.visible) {
+    //     return null
+    // }
     
     return (
-        <div className={`alert alert-${alert.type || "warning"} alert-dismissible`} >
-            <strong>Attention!</strong> 
-            {alert.text}
-            <button onClick={hide} type="button" className="close"  aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+        <CSSTransition
+            in={alert.visible}
+            timeout={{
+                enter: 800,
+                exit: 500
+            }}
+            classNames={'alert'}
+            mountOnEnter
+            unmountOnExit
+        >
+            <div className={`alert alert-${alert.type || "warning"} alert-dismissible`} >
+                <strong>Attention!</strong> 
+                &nbsp;{alert.text}
+                <button onClick={hide} type="button" className="close"  aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </CSSTransition>
     )
 }
 
